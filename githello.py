323c80830fdae6f8f944fda963631ff038fad3ca
Loka Rupesh Reddy <lokarupeshreddy13@gmail.com>
2026-07-08 13:11:21 -0400

    commiting the modfiations made after pulling the lastest commits from the git remote repo

--- githello.py
+++ githello.py
@@ -1,7 +1,7 @@
-N = 5
-for i in range(N):
-  print("Hey This is me in Git/Github")
-
+print("Hey This is me in Git/Github")
 print("adding new line after in remote repo after cloning it to local repo")
-print('''updated the line 5 by pulling it to local repo now adding one more commit to 
-check where is the if the local git status showing if any commits as added to the remote repo")
+print('''updated the line 3 by pulling it to local repo now adding one more commit to 
+check where is the if the local git status showing if any commits as added to the remote repo''')    
+print("This is the new line added in local repo to confirm that the git status cmd is showing available commits to be pushed from the remote to local repo")
+print("Pulled the last commit from the remote repo to local repo ")
+      
