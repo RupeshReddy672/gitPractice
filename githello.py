23b0633ffd1ff677641dea61523995d242037a98
Loka Rupesh Reddy <ramrupesh672@gmail.com>
2026-07-08 13:05:08 -0400

    adding line 6 in remote repo
    Updated line 5 to reflect changes after pulling from remote.

--- githello.py
+++ githello.py
@@ -3,3 +3,5 @@ for i in range(N):
   print("Hey This is me in Git/Github")
 
 print("adding new line after in remote repo after cloning it to local repo")
+print('''updated the line 5 by pulling it to local repo now adding one more commit to 
+check where is the if the local git status showing if any commits as added to the remote repo")
